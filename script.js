c2429d0436ee0c491f2071c02720b365e5b3cc56
ap2212 <53970070+ap2212@users.noreply.github.com>
2024-06-05 11:06:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,16 +35,16 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Graphic designer", "Web designer", "Video Editor",],
+        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Graphic designer", "Web designer", "Video Editor",],
-        typeSpeed: 120,
-        backSpeed: 70,
+        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
         loop: true
     });
 
